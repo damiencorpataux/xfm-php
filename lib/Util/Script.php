e8eca8a6c2a70fb796ebb7b280b971b442b26d21
damiencorpataux <d@mien.ch>
2010-11-05 13:46:14 +0000

    Added a xScript class for creating PHP CLI scripts

---
+++ lib/Util/Script.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * (c) 2010 Damien Corpataux
+ *
+ * LICENSE
+ * This library is licensed under the GNU GPL v3.0 license,
+ * accessible at http://www.gnu.org/licenses/gpl-3.0.html
+ *
+**/
+
+/**
+ * This class is used to create command line scripts
+ * @package xFreemwork
+**/
+abstract class xScript {
+
+    function __construct() {
+        $this->setup();
+    }
+
+    /**
+     * Defines the location of the bootstrap to be used.
+     * Enables to use an overridden bootstrap.
+     * This property is set in the constructor.
+     * @return string Bootstrap absolute location
+     */
+    function bootstrap_location() {
+        return dirname(__file__).'/Bootstrap.php';
+    }
+
+    /**
+     * Setups script components.
+     */
+    function setup() {
+        $this->setup_bootstrap();
+        $this->print_profile_information();
+        $this->run();
+    }
+
+    /**
+     * Setups Bootstrap.
+     */
+    function setup_bootstrap() {
+        require_once($this->bootstrap_location());
+        $b = new xBootstrap();
+    }
+
+    function print_profile_information() {
+        $p = xContext::$profile;
+        $db = xContext::$config->db->toArray();
+        $this->log("Running script with:");
+        $this->log("Profile: {$p}", 1);
+        $this->log("Database: {$db['user']}@{$db['host']}/{$db['database']}", 1);
+        $this->log();
+    }
+
+    /**
+     * The actual user script logic.
+     * This method is to be defined in child class.
+     */
+    abstract function run();
+
+    /**
+     * Outputs a string on stdout, optionally indenting it
+     * @param string $msg The string to output
+     * @param int $indent_level The indentation level
+     */
+    function log($msg = '', $indent_level = 0) {
+        $indent = '';
+        for ($i=0; $i<$indent_level; $i++) $indent .= '*';
+        if (strlen($indent)) $indent .= ' ';
+        print "{$indent}{$msg}\n";
+    }
+}
+
+?>
