1c561e0faeea7ed25ff20906cdefd63e3b617b57
damiencorpataux <d@mien.ch>
2011-03-07 12:34:37 +0000

    docs: working generate.sh script

--- scripts/docs/generate.sh
+++ scripts/docs/generate.sh
@@ -8,7 +8,7 @@ output_dir=../../docs/api/trunk
 rm -rf $output_dir/*
 
 # Generates HTML Documentation
-php $phpdoc_dir/phpdoc -o HTML:frames:earthli -d $source_dir -t $output_dir
+php $phpdoc_dir/phpdoc -o HTML:frames:earthli -d $source_dir -t $output_dir -dn xFreemwork -s > /dev/null
 
 # Renames files (PhpDocumentor bug?)
 # - *.cs -> *.css
