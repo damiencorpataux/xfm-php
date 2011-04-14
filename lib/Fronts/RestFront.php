ae282a2834a2ac6b72ad5cc99ab399d5c466b5df
damiencorpataux <d@mien.ch>
2011-04-14 13:28:58 +0000

    xRestFront: added mimetypes header output + post body decoders

--- lib/Fronts/RestFront.php
+++ lib/Fronts/RestFront.php
@@ -24,6 +24,14 @@ class xRestFront extends xFront {
         'xformat' => 'php'
     );
 
+    /**
+     * Mime types definition for each format
+     */
+    var $mimetypes = array(
+        'xml' => 'text/xml',
+        'json' => 'application/json', // http://www.ietf.org/rfc/rfc4627.txt
+    );
+
     protected function __construct($params = null) {
         parent::__construct($params);
         // Switches encoding if applicable
@@ -44,28 +52,62 @@ class xRestFront extends xFront {
     }
 
     /**
-     * @param mixed PHP variable to encode
+     * Decodes and returns the given data into a PHP array or scalar type.
+     * @param mixed PHP variable to decode.
+     * @return mixed Decoded data
+     */
+    function decode($data) {
+        $format = $this->params['xformat'];
+        $format_method = "decode_{$format}";
+        if (!method_exists($this, $format_method)) throw new xException("REST format input not allowed: {$format}", 501);
+        // TODO: Recodes input stream if necessary
+        $input = $this->$format_method($data);
+        return $input;
+    }
+    function decode_php($data) {
+        return unserialize($data);
+    }
+    function decode_json($data) {
+        if (!function_exists('json_decode')) throw new xException("JSON decoding unavailable", 501);
+        return json_decode($data, true);
+    }
+    function decode_xml($data) {
+        // Warning: get_object_vars() might not be suitable for deep nested XML
+        return @get_object_vars(simplexml_load_string($data));
+    }
+    function decode_xmlrpc($data) {
+        if (!function_exists('xmlrpc_decode')) throw new xException("XMLRPC decoding unavailable", 501);
+        return xmlrpc_decode($data);
+    }
+
+
+    /**
+     * Encodes and returns the given data into the format specified by the xformat parameter.
+     * @param mixed PHP variable to encode.
+     * @return string Encoded data
      */
     function encode($data) {
-        $format_method = "encode_{$this->params['xformat']}";
-        if (!method_exists($this, $format_method)) throw new xException("REST format output not available: {$this->params['xformat']}", 501);
+        $format = $this->params['xformat'];
+        $format_method = "encode_{$format}";
+        if (!method_exists($this, $format_method)) throw new xException("REST format output not available: {$format}", 501);
         $output = $this->$format_method($data);
         // Recodes output stream if necessary
         if ($this->encoding != 'UTF-8') {
             $output = iconv('UTF-8', "{$this->encoding}//TRANSLIT", $output);
         }
+        // Sets HTTP mime type
+        $mime = $this->mimetypes[$format] ? $this->mimetypes[$format] : 'text/plain';
+        header ("Content-Type: {$mime}; charset={$this->encoding}");
+        // Returns output
         return $output;
     }
-
     function encode_php($data) {
-        return var_export($data, true);
+        return serialize($data);
     }
-
     function encode_json($data) {
         if (!function_exists('json_encode')) throw new xException("JSON encoding unavailable", 501);
         return json_encode($data);
     }
-
     function encode_xml($data) {
         $result = $this->encode_xml_nodes($data);
         if ($this->xml_root_node) {
@@ -75,21 +117,22 @@ class xRestFront extends xFront {
         } else {
             $xml = $result;
         }
-        return "<?xml version=\"1.0\" encoding=\"{$this->encoding}\"?>\n{$xml}";
+        return "<?xml version=\"1.0\" encoding=\"{$this->encoding}\"?>\r\n{$xml}";
     }
     function encode_xml_nodes($data) {
         if (!is_array($data)) return $data;
         $r = '';
-        foreach ($data as $key => $value) {
-            $open_tag = $key;
-            $close_tag = array_shift(explode(' ', $key));
+        foreach ($data as $tag => $value) {
+            // Extracts tag:
+            $open_tag = $tag;
+            $close_tag = array_shift(explode(' ', $tag));
             if (is_array($value)) $value = $this->encode_xml_nodes($value);
-            if (is_numeric($key)) $open_tag = $close_tag = $this->xml_default_node;
+            else $value = "<![CDATA[{$value}]]>";
+            if (is_numeric($tag)) $open_tag = $close_tag = $this->xml_default_node;
             $r .= "<{$open_tag}>{$value}</{$close_tag}>";
         }
         return $r;
     }
-
     function encode_xmlrpc($data) {
         if (!function_exists('xmlrpc_encode')) throw new xException("XMLRPC encoding unavailable", 501);
         return xmlrpc_encode($data);
