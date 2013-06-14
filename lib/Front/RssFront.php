061ef625c0714cd9369f9cf1ba32b2c885106a94
Damien Corpataux <d@mien.ch>
2013-06-14 13:27:35 +0200

    Fixed xRssFront class docblock

--- lib/Front/RssFront.php
+++ lib/Front/RssFront.php
@@ -1,5 +1,17 @@
 <?php
+/*
+ * (c) 2012 Damien Corpataux
+ *
+ * LICENSE
+ * This library is licensed under the GNU GPL v3.0 license,
+ * accessible at http://www.gnu.org/licenses/gpl-3.0.html
+ *
+**/
 
+/**
+ * Front controller class for RSS feed.
+ * @package xFreemwork
+**/
 class xRssFront extends xApiFront {
 
     var $xml_root_node = 'rss version="2.0"';
