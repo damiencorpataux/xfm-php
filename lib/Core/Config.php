d09157ea5fceb7f523e72fc326085bc9987fd4fe
Damien Corpataux <d@mien.ch>
2013-07-10 17:31:40 +0200

    Fixed xZend_Config @package

--- lib/Core/Config.php
+++ lib/Core/Config.php
@@ -24,8 +24,7 @@
  */
 
 /**
- * @category   Zend
- * @package    xZend_Config
+ * @package    xFreemwork
  * @version    $Id: Config.php 23775 2011-03-01 17:25:24Z ralph $
  * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
