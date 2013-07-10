2f42987b3e7c2a8f8bc302518ce1363737d50aab
Damien Corpataux <d@mien.ch>
2013-07-10 17:37:23 +0200

    Fixed xZend_Config @package (2nd pass)

--- lib/Core/Config.php
+++ lib/Core/Config.php
@@ -484,7 +484,7 @@ class xZend_Config implements Countable, Iterator
 
 /**
  * @category   Zend
- * @package    xZend_Config
+ * @package    xFreemwork
  * @version    $Id: Ini.php 23775 2011-03-01 17:25:24Z ralph $
  * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
