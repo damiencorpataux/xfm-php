47beca7b1e41c741bc120c1f0755507103ccb3f8
Damien Corpataux <d@mien.ch>
2014-03-13 10:13:02 +0100

    Created a lib from database deploy script and phpunit tests runner

---
+++ lib/Script/DeployDatabase.php
@@ -0,0 +1,103 @@
+<?php
+/*
+ * (c) 2012 Damien Corpataux
+ *
+ * LICENSE
+ * This library is licensed under the GNU GPL v3.0 license,
+ * accessible at http://www.gnu.org/licenses/gpl-3.0.html
+ *
+**/
+
+require_once(dirname(__file__).'/../Util/Script.php');
+
+/**
+ * Executes .sql files present in the sql/ directory (alphabetical order).
+ * @package scripts
+**/
+class DeployDatabaseScript extends xScript {
+
+    public $sql_path;
+
+    function init() {
+        $this->sql_path = xContext::$basepath.'/sql';
+    }
+
+    /**
+     * Setups Bootstrap with database selection fault tolerance.
+     * @see xScript::setup_bootstrap()
+     */
+    function setup_bootstrap() {
+        try {
+            parent::setup_bootstrap();
+        } catch (Exception $e) {
+            // Prevent outputting buffer started in xScript::setup()
+            @ob_end_clean();
+            // Manages null $db resource if configured database does not yet exist
+            if (!$e->getMessage() == 'Could not select database') throw $e;
+            // Sets config->db->database to null temporarily
+            // to allow running xBootstrap::setup()
+            $dbname = xContext::$config->db->database;
+            xContext::$config->db->database = null;
+            parent::setup_bootstrap();
+            // Restores config->db->database value
+            xContext::$config->db->database = $dbname;
+        }
+    }
+
+    function run() {
+        $this->log("Processing files in {$this->sql_path}");
+        foreach($this->get_files_to_execute() as $file) {
+            $this->log("Executing {$file}", 1);
+            // Passes .sql file through xView
+            // (allows using templated SQL for eg. variable database name)
+            $view = xView::load(null);
+            $view->path = xContext::$basepath;
+            $view->default_tpl = $file;
+            // Renders SQL
+            $sql = $view->render();
+            // Executes SQL
+            try {
+                // TODO: ensure splitting by ";\n" will never split inside an sql statement
+                //       use a regex /;(\s)$/
+                //       -OR- use CLIENT_MULTI_STATEMENTS (http://www.php.net/manual/fr/function.mysql-query.php#91669)
+                // ---
+                // Allows multiple statements in a single file,
+                // discarding empty statements
+                $statements = array_filter(array_map('trim', explode(";", $sql)));
+                foreach ($statements as $statement) xModel::q($statement);
+            } catch (Exception $e) {
+                $this->log("Failed executing '{$file}'", 2);
+                $this->log();
+                throw $e;
+            }
+        }
+    }
+
+    /**
+     * Returns an array containing the files to be parsed
+     * @return array
+     */
+    function get_files_to_execute() {
+        $filenames = xUtil::cascade_dir($this->sql_path, 'sql');
+        foreach ($filenames as &$filename) {
+            $filename = substr($filename, strlen(xContext::$basepath.'/'));
+        }
+        return $filenames;
+    }
+
+    function help() {
+        $files = implode(
+            "\n",
+            array_map(
+                function($e) { return "* {$e}"; },
+                $this->get_files_to_execute()
+            )
+        );
+        return array(
+            "Executes alphabetically all the .sql files present",
+            "in '{$this->sql_path}' and its subdirectories:",
+            null,
+            $files
+        );
+    }
+}
---
+++ unittests/lib/PhpUnit.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * (c) 2012 Damien Corpataux
+ *
+ * Licensed under the GNU GPL v3.0 license,
+ * accessible at http://www.gnu.org/licenses/gpl-3.0.html
+ *
+**/
+
+/**
+ * PHPUnit tests CLI runner.
+ * @package xUnittesting
+ */
+class xPhpUnit {
+
+    function __construct() {
+        $this->setup_phpunit();
+        $this->setup_xfm();
+        $this->setup_custom();
+        // PHPUnit autorun
+        if (PHP_SAPI==='cli') PHPUnit_TextUI_Command::main();
+    }
+
+    /**
+     * Returns the base path to PHPUnits vendors path.
+     * Override this to suit your custom project structur, if needed.
+     */
+    function xfm_path() {
+        return __dir__.'/../..';
+    }
+
+    /**
+     * Returns the base path to PHPUnits vendors path.
+     * @see xfm_path()
+     */
+    function phpunit_path() {
+        return $this->xfm_path().'/unittests/vendors';
+    }
+
+    /**
+     * Sets PHPUnits include path and includes Autoload.
+     * @see xfm_path()
+     */
+    function setup_phpunit() {
+        // Requires PHPUnit dependancies (using existing xfm submodules)
+        $phpunit_path = $this->phpunit_path();
+        $paths = array(
+            "{$phpunit_path}/phpunit/",
+            "{$phpunit_path}/php-file-iterator/",
+            "{$phpunit_path}/php-code-coverage/",
+            "{$phpunit_path}/php-token-stream/",
+            "{$phpunit_path}/php-text-template/",
+            "{$phpunit_path}/php-timer/",
+            "{$phpunit_path}/phpunit-mock-objects/"
+        );
+        foreach ($paths as $path) set_include_path(
+            get_include_path() . PATH_SEPARATOR . $path
+        );
+        // Requires PHPUnit library
+        require_once "PHPUnit/Autoload.php";
+    }
+
+    /**
+     * Simply loads xfm-specific xPHPUnit_Framework_TestCase.
+     */
+    function setup_xfm() {
+        // Requires xfm custom xPHPUnit_Framework_TestCase
+        $xfm_path = $this->xfm_path();
+        require_once "{$xfm_path}/unittests/lib/PHPUnit_Framework_TestCase.php";
+    }
+
+    /**
+     * Hook for setting up your custom stuff.
+     * (eg. custom PHPUnit_Framework_TestCase classes)
+     */
+    function setup_custom() {
+        // Requires project-specific xPHPUnit_Framework_TestCase child classes
+        // require_once __dir__.'/lib/myPHPUnit_Framework_TestCase.php';
+    }
+}
